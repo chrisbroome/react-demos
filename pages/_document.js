625c723e8d12f39782dff757c95df6e9a2fae393
Chris Broome <chrisbroome7@gmail.com>
2019-02-11 20:21:13 -0500

    Add custom _document

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+// ./pages/_document.js
+import Document, {Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return {...initialProps}
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
